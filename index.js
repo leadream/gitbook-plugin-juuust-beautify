3d8f2aa9b411100a2faf41df0b6367957ecb46ec
Poojan Shrestha <poojans@gmail.com>
2015-05-19 08:34:58 +0545

    Removed deprecated hooks.

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ module.exports = {
     book: {
         assets: "./book",
         js: [
-            "test.js"
+            "toggle.js"
         ],
         css: [
-            "test.css"
+            "toggle.css"
         ],
         html: {
             "html:start": function() {
@@ -33,44 +33,7 @@ module.exports = {
         // This is called after the book generation
         "finish": function() {
             //console.log("finish!");
-        },
-
-        // The following hooks are called for each page of the book
-        // and can be used to change page content (html, data or markdown)
-
-
-        // Before parsing markdown
-        "page:before": function(page) {
-            // page.path is the path to the file
-            // page.content is a string with the file markdown content
-
-            // Example:
-            //page.content = "# Title\n" + page.content;
-
-            return page;
-        },
-
-        // Before html generation
-        "page": function(page) {
-            // page.path is the path to the file
-            // page.content is a list of parsed sections
-
-            // Example:
-            //page.content.unshift({type: "normal", content: "<h1>Title</h1>"})
-
-            return page;
-        },
-
-        // After html generation
-        "page:after": function(page) {
-            // page.path is the path to the file
-            // page.content is a string with the html output
-
-            // Example:
-            //page.content = "<h1>Title</h1>\n" + page.content;
-            // -> This title will be added before the html tag so not visible in the browser
-
-            return page;
         }
+
     }
 };
