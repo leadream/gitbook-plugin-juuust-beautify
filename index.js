24c1ad8edb9f48d4d7b884eafa529f509a25ae4a
Poojan Shrestha <poojans@gmail.com>
2015-05-19 16:07:04 +0545

    Fixed toggle for gitbook build generated books.

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
 module.exports = {
-    book: {
-        assets: "./book",
-        js: [
-            "toggle.js"
-        ],
-        css: [
-            "toggle.css"
-        ],
-        html: {
-            "html:start": function() {
-                return "<!-- Start book "+this.options.title+" -->"
-            },
-            "html:end": function() {
-                return "<!-- End of book "+this.options.title+" -->"
-            },
+  website: {
+    assets: "./book",
+    js: [
+      "toggle.js"
+    ],
+    css: [
+      "toggle.css"
+    ],
+    html: {
+      "html:start": function() {
+        return "<!-- Start book "+this.options.title+" -->"
+      },
+      "html:end": function() {
+        return "<!-- End of book "+this.options.title+" -->"
+      },
 
-            "head:start": "<!-- head:start -->",
-            "head:end": "<!-- head:end -->",
+      "head:start": "<!-- head:start -->",
+      "head:end": "<!-- head:end -->",
 
-            "body:start": "<!-- body:start -->",
-            "body:end": "<!-- body:end -->"
-        }
-    },
-    hooks: {
-        // For all the hooks, this represent the current generator
-
-        // This is called before the book is generated
-        "init": function() {
-            //console.log("init!");
-        },
-
-        // This is called after the book generation
-        "finish": function() {
-            //console.log("finish!");
-        }
-
+      "body:start": "<!-- body:start -->",
+      "body:end": "<!-- body:end -->"
     }
+  },
+  hooks: {
+    // For all the hooks, this represent the current generator
+
+    // This is called before the book is generated
+    "init": function() {
+      //console.log("init!");
+    },
+
+    // This is called after the book generation
+    "finish": function() {
+      //console.log("finish!");
+    }
+
+  }
 };
