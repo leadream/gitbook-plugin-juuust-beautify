db1a2f7e162a9e2edd0de38212b13cd605f3af4f
Poojan Shrestha <poojans@gmail.com>
2015-05-19 08:48:36 +0545

    Refactor.

--- book/toggle.js
+++ book/toggle.js
@@ -1,21 +1,21 @@
 require(["gitbook", "jQuery"], function(gitbook, $) {
 
+    function expand(chapter) {
+      chapter.show();
+      if (chapter.parent().attr('class') != 'summary'
+         && chapter.parent().attr('class') != 'book-summary'
+         && chapter.length != 0
+         ) {
+        expand(chapter.parent());
+      }
+    }
+
     gitbook.events.bind("page.change", function() {
         $('li.chapter').children('ul.articles').hide();
         $chapter = $('li[data-path="' + window.location.pathname.substr(1) + '"]');
         $children = $chapter.children('ul.articles');
         $parent = $chapter.parent();
         $siblings = $chapter.siblings().children('ul.articles');
-
-        function expand(chapter) {
-          chapter.show();
-          if (chapter.parent().attr('class') != 'summary'
-             && chapter.parent().attr('class') != 'book-summary'
-             && chapter.length != 0
-             ) {
-            expand(chapter.parent());
-          }
-        }
 
         expand($chapter);
 
@@ -24,8 +24,4 @@ require(["gitbook", "jQuery"], function(gitbook, $) {
         }
     });
 
-    //gitbook.events.bind("exercise.submit", function() {
-        //// do something
-        //console.log('exercise.submit');
-    //});
 });
