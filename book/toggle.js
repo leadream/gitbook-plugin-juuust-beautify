c858aa25443a89820ec3b92688b6850a98090320
Poojan Shrestha <poojans@gmail.com>
2015-05-19 16:17:26 +0545

    Indentation.

--- book/toggle.js
+++ book/toggle.js
@@ -2,12 +2,13 @@ require(["gitbook", "jQuery"], function(gitbook, $) {
 
   function expand(chapter) {
     chapter.show();
-    if (chapter.parent().attr('class') != 'summary'
-        && chapter.parent().attr('class') != 'book-summary'
-      && chapter.length != 0
-       ) {
-         expand(chapter.parent());
-       }
+
+    if (chapter.parent().attr('class') != 'summary' &&
+      chapter.parent().attr('class') != 'book-summary' &&
+      chapter.length != 0
+    ) {
+      expand(chapter.parent());
+    }
   }
 
   gitbook.events.bind("page.change", function() {
